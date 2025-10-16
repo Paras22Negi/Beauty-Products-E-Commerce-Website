604b49c40e7fbb2563652fa415b09887e8be20bc
Paras Negi <paras22negi@gmail.com>
2025-10-16 15:18:37 +0530

    style update

--- Frontend/src/Pages/Header.jsx
+++ Frontend/src/Pages/Header.jsx
@@ -122,6 +122,7 @@ function Header() {
                     <img
                       src="https://marscosmetics.in/cdn/shop/files/Match-Stick_500x_8ed33da6-9c36-4d70-80c4-d70911ce086e.webp?v=1721369528&width=500"
                       alt="New Arrivals"
+                      className="w-55 h-auto transform transition-transform duration-300 hover:scale-105"
                     />
                   </div>,
                 ],
@@ -133,6 +134,7 @@ function Header() {
                     <img
                       src="https://marscosmetics.in/cdn/shop/files/Artboard_1_382a6a68-187a-4167-b228-ae36da46c664.png?v=1721370285&width=500"
                       alt="Best Sellers"
+                      className="w-55 h-auto transform transition-transform duration-300 hover:scale-105"
                     />
                   </div>,
                 ],
@@ -146,18 +148,12 @@ function Header() {
                   {section.items.map((item, j) => (
                     <li
                       key={j}
-                      className={
-                        typeof item === "string"
-                          ? "hover:text-pink-400 cursor-pointer transition-colors"
-                          : "w-55 h-auto transform transition-transform duration-300 hover:scale-105" 
-                      }
+                      className="hover:text-pink-400 cursor-pointer transition-colors"
                     >
                       {item}
                     </li>
                   ))}
-                  <li
-                    className= "hover:text-pink-400 cursor-pointer transition-colors"
-                  >
+                  <li className="hover:text-pink-400 cursor-pointer transition-colors">
                     <u>View All</u>
                   </li>
                 </ul>

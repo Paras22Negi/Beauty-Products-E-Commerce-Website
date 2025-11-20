003d1f248b7e7d7211ffac2230d92aaf22e822dd
Paras Negi <paras22negi@gmail.com>
2025-11-20 11:11:10 +0530

    cors link

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,7 +10,7 @@ const Port = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL1, process.env.FRONTEND_URL2],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],

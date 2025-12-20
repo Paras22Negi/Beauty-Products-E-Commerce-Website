867ab4f4e3836f58f6ae03fb224c01ebbcb22936
Paras Negi <paras22negi@gmail.com>
2025-12-20 12:36:46 +0530

    deploy error

--- Backend/src/Routes/Order.Routes.js
+++ Backend/src/Routes/Order.Routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authenticate from "../middleware/authenticat.js";
 const router = express.Router();
-import * as orderController from "../controllers/order.controller.js";
+import * as orderController from "../Controllers/order.Controller.js";
 
 router.post("/", authenticate, orderController.createOrder);
 router.get("/user", authenticate, orderController.OrderHistory);

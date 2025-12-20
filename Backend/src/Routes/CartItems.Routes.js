64cac8111d2e0459ff18361136c948339809b4b4
Paras Negi <paras22negi@gmail.com>
2025-12-20 12:32:09 +0530

    deploy error

--- Backend/src/Routes/CartItems.Routes.js
+++ Backend/src/Routes/CartItems.Routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authenticate from "../middleware/authenticat.js";
 const router = express.Router();
-import * as cartItemController from "../controllers/cartItem.controller.js";
+import * as cartItemController from "../Controllers/cartItem.Controller.js";
 
 router.put("/:id", authenticate, cartItemController.updateCartItem);
 router.delete("/:id", authenticate, cartItemController.removeCartItem);
